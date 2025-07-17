ec2b0cbbc30cbccd8cd7019c6b2a291c98d9b41a
Francis <franciskarani15@gmail.com>
2025-07-17 14:19:48 +0300

    adds brach

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,6 +6,10 @@ export default function Footer() {
     <footer>
       <h1>Footer</h1>
       <h2>contact us</h2>
+      <p>0748495749</p>
+       <p>0748495749</p>
+        <p>0748495749</p>
+         <p>0748495749</p>
       <p>&copy;2025</p>
       <address>Nairobi Kenya @the Bazaar Plaza</address>
 
