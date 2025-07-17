dbd32ddb7a59d33ca65332d1c1da2b2649044854
Francis <franciskarani15@gmail.com>
2025-07-17 12:04:02 +0300

    changes

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
+    <>
+    <footer>
       <h1>Footer</h1>
-    </div>
+      <h2>contact us</h2>
+      <p>&copy;2025</p>
+      <address>Nairobi Kenya @the Bazaar Plaza</address>
+
+    </footer>
+    </>
   )
 }
